f038507218d609b6ab0fa6b00ba8365810526e57
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-08 19:57:28 -0500

    master

--- tree_lca.c
+++ tree_lca.c
@@ -41,33 +41,49 @@ T*find_num(T*r,int target)
 	else{find_num(r->r,target);}	
 }
 
-int M_p = 0, N_p = 0;
+int F = 0, S = 0;
 
-void endpts(T*r,T*M,T*N)
+T*endpts(T*r,T*M,T*N)
 {
-	if (!r)return;
-	if (r==M) M_p = 1;
-	if (r==N) N_p = 1;
+	if(!r)return;
+	if (r==M)F=1;
+	if (r==N)S=1;
+	if(F&&S)return;	
 	endpts(r->l,M,N);
+	if(F&&S)return;
 	endpts(r->r,M,N);
+	if(F&&S)return;
 }
 
 T*lca(T*p,T*r,T*M,T*N)
 {
+	if(!r){return 0;}
+	
+	F=S=0;
+
 	endpts(r,M,N);
-	if(!M_p&&!N_p){M_p=N_p=0;return;}
-	else if(M_p&&N_p){M_p=N_p=0;lca(r,r->l,M,N);M_p=N_p=0;lca(r,r->r,M,N);}
-	else if(M_p^N_p){return p;}
+	
+	if(F==0 && S==0){return 0;}
+	
+	else if(F==1^S==1){return p;}
+	
+	if (lca(r,r->l,M,N)) return lca(r,r->l,M,N);
+
+	if (lca(r,r->r,M,N)) return lca(r,r->r,M,N);
+		
+	return 0;	
+		
 }
 
 main(int argc,char**argv)
 {
 	T*r=(T*)calloc(1,sizeof(T));
-	r->v=strtol(argv[1],0,10);
-	char**argv_p=&argv[1];
+	r->v=strtol(argv[3],0,10);
+	char**argv_p=&argv[3];
 	while(*++argv_p){insert_root(&r,strtol(*argv_p,0,10));}
-	argv_p=&argv[0];
-	T*M=find_num(r,strtol(argv[2],0,10));
-	T*N=find_num(r,strtol(argv[3],0,10));
-	printf("lca: %p lca_v:%d\n",lca(0,r,M,N),lca(0,r,M,N)->v);	
+	T*M=find_num(r,strtol(argv[1],0,10));
+	T*N=find_num(r,strtol(argv[2],0,10));
+	endpts(r,M,N);
+	printf("F:%d S:%d\n",F,S);
+	printf("lca: %p lca_v: %d\n",lca(0,r,M,N),lca(0,r,M,N)->v);	
 }
