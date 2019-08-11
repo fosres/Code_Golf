0cba3c92f455f29e37630af5395a89757296967e
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-10 21:11:59 -0500

    master

--- defanged_ip.c
+++ defanged_ip.c
@@ -1,8 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
 char*defanged(char*s,size_t len)
 {
-	static unsigned char defanged[len];
+	
+	static unsigned char defanged[12];
 
-	static unsigned char * d_p = defanged;
+	memset(defanged,0x0,len);
 
-	while
+	unsigned char * d_p = defanged;
+	
+	while ( *s != 0 )
+	{
+		if (*s=='.'){*d_p++='[';*d_p++='.';*d_p++=']';s++;}
+		else {*d_p++ = *s++;}
+
+	}
+
+	*d_p = 0;
+
+	return defanged;
+}
+
+int main(int argc,char**argv)
+{
+	printf("%s\n",defanged(argv[1],strnlen(argv[1],11)));
+
+	return 0;
 }
