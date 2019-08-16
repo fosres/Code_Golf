9331bdcbf38283d6caad9a2a1541b8a0392b9077
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-08-16 12:24:41 -0500

    master

--- tree_exterior.c
+++ tree_exterior.c
@@ -30,8 +30,16 @@ void insert_root(T ** root, int in)
   }
 }
 
-i(T*r){if(!r){return;}i(r->l);if(!r->l&&!r->r){printf("%d ",r->v);}i(r->r);}l(T*r){if(!r){return;}if(r->l){printf("%d ",r->v);}l(r->l);}ri(T*r){if(!r){return;}ri(r->r);if(r->r){printf("%d ",r->v);}}e(T*r){l(r);i(r);ri(r->r);}
-
+i(T*r)
+{
+	if(!r){return;}
+	i(r->l);
+	if(!r->l&&!r->r){printf("%d ",r->v);}
+	i(r->r);
+}
+l(T*r){if(!r){return;}if(r->l){printf("%d ",r->v);}l(r->l);}
+ri(T*r){if(!r)return;ri(r->r);if(r->l||r->r){printf("%d ",r->v);}}
+e(T*r){l(r);i(r);ri(r->r);}
 main(int argc,char**argv)
 {
 	T*r=(T*)calloc(1,sizeof(T));
